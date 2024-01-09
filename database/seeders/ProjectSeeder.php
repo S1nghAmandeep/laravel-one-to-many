da52e49aed3b5f0c964cd0bc20fb2ba33ad42464
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-09 17:11:44 +0100

    add category id to projects table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Project;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,6 +16,9 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
 
+        $categories = Category::all();
+        $ids = $categories->pluck('id');
+
         for ($i = 0; $i < 10; $i++) {
 
             $new_Project = new Project();
@@ -22,7 +26,7 @@ class ProjectSeeder extends Seeder
             $new_Project->link_project = $faker->url();
             $new_Project->language = $faker->randomElement(['html', 'css', 'javascript', 'php']);
             $new_Project->description = $faker->sentence();
-
+            $new_Project->category_id = $faker->optional()->randomElement($ids);
             $new_Project->save();
         }
     }
