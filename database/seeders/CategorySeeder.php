cd5ad17bfe8c2ff6aff6d138207346627dd475c3
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-09 16:01:10 +0100

    add migration and seeder of category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $categories = ['Frontend', 'Backend', 'Design', 'FullStack'];
+
+        foreach ($categories as  $category_name) {
+            $new_category = new Category();
+            $new_category->name = $category_name;
+            $new_category->slug = Str::slug($category_name);
+
+            $new_category->save();
+        }
     }
 }
